b1a4e96cf5eca49e8560790e4dcf8ded3ea4d5c9
Yanuar Rahmat Yoga Pradana <54838397+yanuarpradanaa@users.noreply.github.com>
2021-06-29 11:39:50 +0700

    Commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name" : "Invoice Prima Print ",
+    "category" : "Print out Invoice",
+    "version" : "2.0",
+    "depends" : [
+        'account',
+        'stock',
+    ],
+    "description": """
+Print Out Invoice""",
+    "author" : "Satusoft",
+    "data" : [
+        'report/inv_pp.xml',
+        'report/inv_pp_document.xml',
+    ],
+    'installable' : True,
+    'active' : False,
+}
